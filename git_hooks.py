9803edea9e9f4a2d79be0c3b0a1b1a3e9273e5d9
iyurev <idyurev@gmail.com>
2018-03-31 17:08:18 +0300

    Add similar funcrions

--- git_hooks.py
+++ git_hooks.py
@@ -9,6 +9,21 @@ class GitProject():
         self._project_git_url = project_git_url
         self.default_clone_to = './'
         self._project_name = None
+        self.repo_obj = None
+        self.to_path = self.default_clone_to + self.name_from_url()
+        self.repo_obj = self.repo_obj()
+        if not  self.repo_obj:
+            print 'NOT INIT REPO OBJ!!!'
+
+    def init_repo_obj(self):
+        if os.path.exists(self.to_path) and os.path.isdir(self.to_path):
+            repo_obj = Repo(self.to_path)
+            if isinstance(repo_obj, Repo):
+                return repo_obj
+        else:
+            repo_obj = self.clone(to_path=self.to_path)
+            return  repo_obj
+
     def show_url(self):
         print(self._project_git_url)
 
@@ -19,16 +34,19 @@ class GitProject():
         return self._project_name
 
     def clone(self, flush_repo=False, to_path=None):
-        if not to_path:
-            self.to_path = self.default_clone_to +  self.name_from_url()
-        else:
+        if to_path:
             self.to_path = to_path
-
         if os.path.exists(self.to_path) and os.path.isdir(self.to_path) and flush_repo:
             rmtree(self.to_path)
+        self.repo_obj = Repo.clone_from(url=self._project_git_url, to_path=self.to_path)
 
-        _clone = Repo.clone_from(url=self._project_git_url, to_path=self.to_path)
-        print(_clone)
+    def add_remote(self, remote_addr=None, remote_name = None):
+        if remote_addr and remote_name:
+            if not self.repo_obj:
+                self.repo_obj = Repo(self.to_path)
+            if self.repo_obj:
+                self.repo_obj.create_remote(remote_name, remote_addr)
+
 
 
 
@@ -37,9 +55,9 @@ class GitProject():
 project_url='https://github.com/badeveloper/testproject.git'
 
 demo_proj = GitProject(project_git_url=project_url)
-demo_proj.show_url()
-print(demo_proj.name_from_url())
-demo_proj.clone(flush_repo=True)
+
+#demo_proj.clone(flush_repo=True)
+#demo_proj.add_remote('https://msk-dpro-gka003.x5.ru:8443/invoice_discounting/msa-service-ucd.git', 'sinimex')
 
 # #proj = GitProject(project_http_url='https://github.com/badeveloper/testproject.git')
 # #proj.show_url()
